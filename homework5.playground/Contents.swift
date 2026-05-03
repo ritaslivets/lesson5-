080ef7ddc928f5982027a2f26c9428c2eeb5d30e
ritaslivets <rita4kasliva2007@gmail.com>
2026-05-03 20:30:01 +0300

    done2

--- homework5.playground/Contents.swift
+++ homework5.playground/Contents.swift
@@ -1,3 +1,4 @@
+//MARK: - 1
 enum Currency: String {
     case usd = "USD"
     case eur = "EUR"
@@ -35,3 +36,45 @@ return amount * rate
 }
 let firstAmount = convertCurrency(amount: 100, from: .usd, to: .eur)
 print (firstAmount ?? "нет данных!")
+
+//MARK: - 2
+enum Grade: Int {
+    case A = 100
+    case B = 90
+    case C = 70
+    case D = 40
+    case F = 20
+}
+func getLetterGrade(score: Int) -> Grade? {
+    if score <= 0 || score > 100 {
+        return nil
+    }
+    switch score {
+    case 91...100:
+        return .A
+    case 71...90:
+        return .B
+    case 41...70:
+        return .C
+    case 21...40:
+        return .D
+    default:
+        return .F
+    }
+}
+let score1 = 90
+if let grade = getLetterGrade(score: score1) {
+    print("Балл: \(score1) = \(grade)")
+} else {
+    print("Неверный балл!")
+}
+
+func printExamResult(name: String, score: Int) -> String {
+    let grade = getLetterGrade(score: score)
+    return grade.map { "\(name) получил \($0) (Балл: \(score))" }
+    ?? "\(name) получил недопустимый балл: \(score)"
+}
+let stedunt1 = printExamResult(name: "Misha", score: 94)
+print(stedunt1)
+let studet2 = printExamResult(name: "Alex", score: -3)
+print(studet2)
